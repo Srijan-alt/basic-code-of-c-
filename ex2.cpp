58cbdd5da145cb445a5df4e53e59e0c5ce440c1e
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2020-01-10 18:30:31 +0530

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   string pass;
+   string real_pass="shashwat";
+   cout<<"idiot  enter the password now!!!"<<endl;
+   cin>>pass
+   ;
+   if(pass==real_pass)
+   {
+       cout<<"Your are right nigga"<<endl;
+   }
+   else
+   {
+       cout<<"told ya idiot nigga"<<endl;
+   }
+   return 0;
+
+}
