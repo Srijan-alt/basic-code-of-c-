922686de034d6b2f7cbfd609a764fa2e578a9b9a
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2020-01-14 18:49:16 +0530

    Create ex6.cpp

---
+++ ex6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        cout<<"hello\n";
+    }
+    cout<<i<<flush;
+    /*flush is for end the cout operation and is also does not give the courser to go the next line
+    *I could have used cout<<"hello\n";
+    *this line is just to show that how to use comment for big paragraph
+    */
+    return 0;
+}
