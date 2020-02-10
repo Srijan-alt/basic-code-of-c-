65f6ea076f5d7ccaf15956b5fa9f708874ee0f81
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2020-02-10 22:28:14 +0530

    Create for_loop.cpp
    This code is a example of for loop

---
+++ for_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    string text[]={"tony","pintu","chintu","sonny"};
+    cout<<"name of the kids:"<<endl;
+    for(i=0;i<4;i++)
+    {
+        cout<<text[i]<<endl;
+    }
+}
